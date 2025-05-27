c4805055eb7c147623ce497749b19a1a7cf3fb07
BulaSete <36999228+xkatld@users.noreply.github.com>
2025-05-28 02:56:59 +0800

    Delete frontend directory

--- frontend/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'http://your_production_backend_url/api',
-  apiKey: 'your_production_api_key'
-};
--- frontend/src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8000/api',
-  apiKey: 'a_very_secret_api_key_12345'
-};
