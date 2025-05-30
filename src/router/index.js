6945feaac04b4e0f3d9ad8929d42e0cc4ec9c04f
BulaSete <36999228+xkatld@users.noreply.github.com>
2025-05-31 02:51:05 +0800

    Create index.js

---
+++ src/router/index.js
@@ -0,0 +1,27 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import ManageBackendsView from '../views/ManageBackendsView.vue'
+import ContainerListView from '../views/ContainerListView.vue'
+
+const routes = [
+  {
+    path: '/',
+    redirect: '/manage-backends'
+  },
+  {
+    path: '/manage-backends',
+    name: 'ManageBackends',
+    component: ManageBackendsView
+  },
+  {
+    path: '/containers',
+    name: 'ContainerList',
+    component: ContainerListView
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes
+})
+
+export default router
