4076b2b6f2fa15b58df1baf47eecfd928e45e0b5
BulaSete <36999228+xkatld@users.noreply.github.com>
2025-06-01 01:23:54 +0800

    Update App.jsx

--- public/src/App.jsx
+++ public/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useCallback, useRef } from 'react';
 import { ApiService } from './ApiService';
-import RFB from '@novnc/novnc/core/rfb';
+import RFB from '@novnc/novnc/lib/rfb.js';
 import 'xterm/css/xterm.css';
 
 
